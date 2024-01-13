8055f3837cfed9d13d1a79ad26bedd644f840e64
kelvinnmuia <kelvinnmuia@gmail.com>
2024-01-13 03:40:15 -0800

    created fabric script to deploy the web static files

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -13,7 +13,8 @@ def do_clean(number=0):
     Args:
         number (int): The number of archives to keep
 
-    if number is 0 or 1, keeps only the most recent archive. If number is 2, keeps the most and second-most recent archives, etc
+    if number is 0 or 1, keeps only the most recent archive. If number
+    is 2, keeps the most and second-most recent archives, etc
     """
     number = 1 if int(number) == 0 else int(number)
 
